a75f6cb2986a51a482686b01b0e3fd6b9042a93d
Arpit Kogta <arpitkogta2001@gmail.com>
2021-06-05 01:58:38 +0530

    vaccinfolft

--- src/Extension.js
+++ src/Extension.js
@@ -6,29 +6,29 @@ function Extension() {
         <h1>Extensions</h1>
         <div class="feature-card card-lg light-bordered hover-inset hey3">
             <div class="card-img">
-                <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" alt="" />
+                <img src="https://i.ytimg.com/vi/G9S5FJsU8Xk/maxresdefault.jpg" alt="https://images.news18.com/ibnlive/uploads/2021/05/1620386207_cowin-security-code.jpeg" />
             </div>
             <div class="cardhey">
                 <div class="card-title  pb-10 pt-10">
-                    <h4>Some title</h4>
+                    <h4>Vaccine-Finder</h4>
                 </div>
                 <div class="card-img-description has-text-left">
-                    <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet cu harum Cu soleat euismod mei, illum soleat ei vis, offendit partiendo intellegebat id vix. ...</span>
-                </div><button type="submit" class="btn-align button hey primary-btn raised">Search</button>
+                    <span class="">https://github.com/sushrut111/cowin-automation-extn</span>
+                </div><button type="button" class="btn-align button hey primary-btn raised"><a href="https://chrome.google.com/webstore/detail/gemdkhkmcnifpfbfnhpbbhageflibppm">Goto-link</a></button>
             </div>
         </div>
 
         <div class="feature-card card-lg light-bordered hover-inset hey3">
             <div class="card-img">
-                <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" alt="" />
+                <img src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202104/cowin_1200gtrg_1200x768.jpeg?TOlaBnZL8HE2gFnLX4Rwx_XVHDzjpKLN&size=1200:675" alt="https://i.ytimg.com/vi/ObexH_UzI5Y/maxresdefault.jpg" />
             </div>
             <div class="cardhey">
                 <div class="card-title  pb-10 pt-10">
-                    <h4>Some title</h4>
+                    <h4>VaccAssistV4</h4>
                 </div>
                 <div class="card-img-description has-text-left">
-                    <span class="">Lorem ipsum dolor sit amet, clita laoreet ne cum. His caelus elet cu harum Cu soleat euismod mei, illum soleat ei vis, offendit partiendo intellegebat id vix. ...</span>
-                </div><button type="submit" class="btn-align button hey primary-btn raised">Search</button>
+                    <span class="">https://github.com/hyper-trophy/VaccASSIST</span>
+                </div><button type="button" class="btn-align button hey primary-btn raised"><a href="https://www.udrop.com/5yjg/VaccAssistV4-5.zip">Goto-link</a></button>
             </div>
         </div>
 
